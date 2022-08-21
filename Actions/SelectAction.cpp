3f73e4c52b3157ba9206bd6877c6fc2c278446c8
Mariam-Ghonaim <s-mariam.ghonaim@zewailcity.edu.eg>
2022-08-21 16:01:41 +0200

    .

--- Actions/SelectAction.cpp
+++ Actions/SelectAction.cpp
@@ -17,15 +17,18 @@ void SelectAction::ReadActionParameters()
 
 	for (int i = (pManager->get_FigCount() - 1); i > -1; i--)
 	{
-		if ((pManager->FigList[i])->WithinMe(v1, pOut))
+		while (!(pManager->FigList[i])->WithinMe(v1, pOut))
 		{
-			pManager->FigList[i]->Draw(pOut);
-			if (pManager->FigList[i]->GetSelectCounter() > 1)
-				pOut->PrintMessage("Number of Selected Figures is " + to_string(pManager->FigList[i]->GetSelectCounter()));
-			else
-				pOut->PrintMessage(pManager->FigList[i]->PrintInfo());
-			break;
+			pIn->GetPointClicked(v1.x, v1.y);
 		}
+		
+		pManager->FigList[i]->Draw(pOut);
+		if (pManager->FigList[i]->GetSelectCounter() > 1)
+		pOut->PrintMessage("Number of Selected Figures is " + to_string(pManager->FigList[i]->GetSelectCounter()));
+		else
+		pOut->PrintMessage(pManager->FigList[i]->PrintInfo());
+		break;
+	
 	}
 		
 }
